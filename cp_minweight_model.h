2ff13244e3d4ae95a68f164884c3af3f90c19e2e
Sabine <sabine.muzellec@protonmail.com>
2020-06-24 17:18:58 +0200

    new objective function

--- cp_minweight_model.h
+++ cp_minweight_model.h
@@ -63,6 +63,9 @@ namespace operations_research{
     */
     class CPModel_MinWeight : public CP_Model{
 
+    private:
+      std::vector<std::vector<IntVar>> sum;
+
     public:
 
       /*
@@ -84,7 +87,7 @@ namespace operations_research{
       Parameters : None
       Output : None
       */
-      void model_declare_objective(){
+      void model_declare_objective(const int &index_example){
         for (size_t l = 1; l < bnn_data.get_layers(); l++) {
           for(size_t i = 0; i < bnn_data.get_archi(l-1); i++) {
             for (size_t j = 0; j < bnn_data.get_archi(l); j++) {
@@ -94,6 +97,21 @@ namespace operations_research{
             }
           }
         }
+        sum.resize(nb_examples);
+        sum[index_example].resize(bnn_data.get_archi(1));
+        for (size_t i = 0; i < bnn_data.get_archi(1); i++) {
+          sum[index_example][i] = cp_model.NewIntVar(Domain(-255*bnn_data.get_archi(0),255*bnn_data.get_archi(0)));
+          std::vector<IntVar> temp(bnn_data.get_archi(0));
+          for (size_t j = 0; j < bnn_data.get_archi(0); j++) {
+            temp[j] = cp_model.NewIntVar(Domain(-255,255));
+            IntVar e_i = cp_model.NewIntVar(Domain(0,255));
+            cp_model.AddEquality(e_i, (int64)bnn_data.get_dataset().training_images[index_example+index_rand][j]);
+            cp_model.AddProductEquality(temp[j], {get_w_ilj(j, 1, i), e_i});
+          }
+          cp_model.AddEquality(sum[index_example][i], LinearExpr::Sum(temp));
+        }
+
+
       }
 
       /* model_output_constraint method
