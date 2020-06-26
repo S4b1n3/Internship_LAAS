32c991909ef7c22e48778f18c5cd6e7e73f489ba
Sabine <sabine.muzellec@protonmail.com>
2020-06-27 01:36:05 +0200

    building datasets

--- bnn-main.cc
+++ bnn-main.cc
@@ -63,9 +63,9 @@ int main(int argc, char **argv) {
 		operations_research::sat::CPModel_MinWeight first_model(architecture, _nb_examples, _prod_constraint, filename);
 		first_model.run(_time, _strategy) ;
 		int status = first_model.print_statistics();
-		first_model.print_solution(first_model.get_response());
-		first_model.print_solution_bis(first_model.get_response());
-		first_model.print_all_solutions() ;
+		//first_model.print_solution(first_model.get_response());
+		//first_model.print_solution_bis(first_model.get_response());
+		//first_model.print_all_solutions() ;
 		if(status == 2 || status == 4){
 			Evaluation test(first_model.get_weights_solution(), first_model.get_data());
 			accuracy_test = test.run_evaluation(true);
